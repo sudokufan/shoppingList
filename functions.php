b202b09a2b1df01682f8eb4ad9d96177cb9ce3c6
sudokufan <alexrichardgeorge@protonmail.com>
2019-11-30 13:40:44 +0000

    wrote displayShoppingList function

--- functions.php
+++ functions.php
@@ -27,3 +27,22 @@ function getShoppingList(PDO $db): array
     $items = $query->fetchAll();
     return $items;
 }
+
+/**
+ * displays shopping list items on front end
+ *
+ * @param array $items indexed array of assoc arrays of items
+ *
+ * @return string div containing display code for front-end
+ */
+function displayShoppingList(array $items): string
+{
+    $result = '';
+
+    foreach ($items as $item) {
+        $result .= '<div>
+                    <h3>' . $item['name'] . '</h3>
+                    </div>';
+    }
+    return $result;
+}
