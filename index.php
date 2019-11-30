6eccc3940de2e73afe9eeed0745116cc85f87cf5
sudokufan <alexrichardgeorge@protonmail.com>
2019-11-30 13:21:25 +0000

    added normalize.css and called in

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ $db = connectDB();
 
 <html lang="en-GB">
 <head>
+    <link rel="stylesheet" type="text/css" href="normalize.css" />
     <link rel="stylesheet" type="text/css" href="styles.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Maydinner</title>
