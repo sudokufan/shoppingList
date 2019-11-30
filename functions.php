93baabe391096e53d79ca148e23f6deb175e560c
sudokufan <alexrichardgeorge@protonmail.com>
2019-11-30 13:17:48 +0000

    wrote connectDB function to connect to database

--- functions.php
+++ functions.php
@@ -1,1 +1,9 @@
 <?php
+
+
+function connectDB(): PDO
+{
+    $db = new PDO('mysql:host=db; dbname=shoppingList', 'root', 'password');
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $db;
+}
