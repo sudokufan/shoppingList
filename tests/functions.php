e65167c155a26914fb218ef5c7ad3d1b67546f97
sudokufan <alexrichardgeorge@protonmail.com>
2019-11-30 15:51:27 +0000

    fixed formatting on tests/functions.php to include all functions in class

--- tests/functions.php
+++ tests/functions.php
@@ -5,19 +5,22 @@ use PHPUnit\Framework\TestCase;
 
 class StackTest extends TestCase
 {
-    public function testSuccessDisplayShoppingList() {
+    public function testSuccessDisplayShoppingList()
+    {
         $expected = '<div>
                     <ul>value</ul>
                     </div>';
-        $input = [['name'=>'value']];
+        $input = [['name' => 'value']];
         $case = displayShoppingList($input);
         $this->assertEquals($expected, $case);
     }
-}
 
-public function testFailureDisplayShoppingList() {
-    $expected = 'Incorrect SQL data; please contact administrator';
-    $input = [['Lemons']];
-    $case = displayShoppingList($input);
-    $this->assertEquals($expected, $case);
+    public function testFailureDisplayShoppingList()
+    {
+        $expected = 'Incorrect SQL data; please contact administrator';
+        $input = [['Lemons']];
+        $case = displayShoppingList($input);
+        $this->assertEquals($expected, $case);
+    }
+
 }
