dcb82ba7326c474c4c1ab9d0e602725b3634af82
sudokufan <alexrichardgeorge@protonmail.com>
2019-11-30 15:42:16 +0000

    wrote success test for displaying shopping list

--- tests/functions.php
+++ tests/functions.php
@@ -2,3 +2,15 @@
 
 require_once ('../functions.php');
 use PHPUnit\Framework\TestCase;
+
+class StackTest extends TestCase
+{
+    public function testSuccessDisplayShoppingList() {
+        $expected = '<div>
+                    <ul>name</ul>
+                    </div>';
+        $input = [['name'=>'value']];
+        $case = displayShoppingList($input);
+        $this->assertEquals($expected, $case);
+    }
+}
